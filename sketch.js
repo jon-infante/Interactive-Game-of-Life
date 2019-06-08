56612749c026bfe98c7ea7cb728de5df6eddf851
Jonathan Infante <jonjoninfante@gmail.com>
2019-06-08 12:30:18 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -146,7 +146,7 @@ class Grid {
   comeAlive(){
   if (mouseIsPressed) {
     var cell = this.cells[mouseColumn][mouseRow];
-    this.isAlive = true;
+    cell.isAlive = true;
   } 
 }
   //Initial random seed
